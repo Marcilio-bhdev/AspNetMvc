a038dbc5d4f0df0b4213614f9ba4d2e389983920
Marcilio-bhdev <marcilio.santos@meta3.com.br>
2020-03-13 15:38:22 -0300

    Sellers Controller

--- SalesWeb/Models/Department.cs
+++ SalesWeb/Models/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,28 @@ namespace SalesWeb.Models
 {
     public class Department
     {
-
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<Saller> Sellers { get; set; } = new List<Saller>();
 
+        public Department() 
+        {
+        }
 
+        public Department(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
 
+        public void AddDepartment(Saller seller)
+        {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sellers.Sum(seller => seller.TotalSales (initial, final));// estou pegando cada vendedor chamanco o total do que ele vendeu e somando com todos os vendedores 
+        }
     }
 }
